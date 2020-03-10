a5cdce947dace49da472425be5d5de6e636fabf1
Roamdev <aroundthefog@gmail.com>
2020-03-10 22:54:05 +0400

    disabled

--- src/CardNumberHolder.js
+++ src/CardNumberHolder.js
@@ -1,9 +1,30 @@
 import React, {Component} from 'react';
+import CardNumberInput from './CardNumberInput';
 
 class CardNumberHolder extends Component {
+  state = {
+    cardNumber: 'govno'
+  }
+  handleChange = (e) => {
+    this.setState({cardNumber: e.target.value})
+  }
+  handleKeyDown = e => {
+    console.log(this.state.cardNumber)
+    console.log(this.state.disabled)
+    if (e.keyCode === 13)
+    this.setState({cardNumber: e.target.value, disabled: true})
+    return <div>well done</div>
+  }
   render() {
     return (
-      <div>CardNumberHolder</div>
+      <>
+        <div>{this.state.cardNumber}</div>
+        <CardNumberInput
+          disabled={this.state.disabled}
+          handleKeyDown={this.handleKeyDown}
+          handleChange={this.handleChange}
+        />
+      </>
     );
   }
 }
--- src/CardNumberInput.js
+++ src/CardNumberInput.js
@@ -1,8 +1,17 @@
-import {Component} from 'react';
+import React, {Component} from 'react';
 
 class CardNumberInput extends Component {
+
   render() {
-    return null;
+    return (
+      <>
+        <input
+          onKeyDown={this.props.handleKeyDown}
+          // onChange={this.props.handleChange}
+          disabled={this.props.disabled}
+        ></input>
+      </>
+    );
   }
 }
 
